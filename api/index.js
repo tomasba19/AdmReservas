6b10df6595cf1032d9d1e7934105f17a800e878e
Tomas Barolo <tomasba19@gmail.com>
2023-11-01 17:58:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const pool = require('./db');
+const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
@@ -9,9 +9,65 @@ app.use(express.json());
 
 //ROUTES//
 
-//crear reserva
+//crear reserva / caba;a
+app.post("/cabanas", async (req, res) => {
+  try {
+    const { nombre, capacidad } = req.body;
+    const newCabana = await pool.query(
+      "INSERT INTO cabanas (nombre, capacidad) VALUES($1, $2) RETURNING *",
+      [nombre, capacidad]
+    );
+    res.json(newCabana.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-//get todas las reservas
+app.post("/reservas", async (req, res) => {
+  try {
+    const {
+      cabana_id,
+      fecha_inicio,
+      fecha_fin,
+      cliente_nombre,
+      cliente_telefono,
+      cliente_dni,
+    } = req.body;
+    const newReserva = await pool.query(
+      "INSERT INTO reservas (cabana_id, fecha_inicio, fecha_fin, cliente_nombre, cliente_telefono, cliente_dni) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        cabana_id,
+        fecha_inicio,
+        fecha_fin,
+        cliente_nombre,
+        cliente_telefono,
+        cliente_dni,
+      ]
+    );
+    res.json(newReserva.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get todas las reservas / cabanias
+app.get("/cabanas", async (req, res) => {
+  try {
+    const allCabanas = await pool.query("SELECT * FROM cabanas");
+    res.json(allCabanas.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.get("/reservas", async (req, res) => {
+  try {
+    const allReservas = await pool.query("SELECT * FROM reservas");
+    res.json(allReservas.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //get una reserva
 
@@ -19,7 +75,6 @@ app.use(express.json());
 
 //borrar reserva
 
-
 app.listen(5000, () => {
-    console.log('Server is running on port 5000')
+  console.log("Server is running on port 5000");
 });
